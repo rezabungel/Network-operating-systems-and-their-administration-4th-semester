2329f7e52e3e757953431dbacf18db842ddfa78a
rezabungel <svyatchenko-arte@mail.ru>
2022-03-30 04:48:31 +0300

    Adding programs 1-3 and the results of their work (except for the result of the first program).

---
+++ Lecture/1 - Example of a dynamic memory allocation program for an array.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int *array = NULL;
+    unsigned length, i;
+
+    printf("Enter length of array: ");
+    scanf("%d", &length);
+
+    if (length > 0)
+    {
+        //При выделении памяти возвращается указатель.
+        //Если память не была выделена, то возвращается NULL
+        if ((array = (int *)malloc(length * sizeof(int))) != NULL)
+        {
+            for (i = 0; i < length; i++)
+            {
+                array[i] = i * i;
+                printf(sizeof(array));
+            }
+        }
+        else
+        {
+            perror("Error: can't allocate memory");
+        }
+    }
+
+    //Если переменная была инициализирована, то очищаем её
+    if (array != NULL)
+    {
+        free(array);
+    }
+    getch();
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Network-operating-systems-and-their-administration-4th-semester
  
+The output of the result of the work of the programs from the lectures.
+
+Program №1
+
+???
+fatal error: conio.h: No such file or directory
+3 | #include <conio.h>
+| ^~~~~~~~~
+compilation terminated.
+???
+
+Program №2
+
+fd = 3
+called read( 3, c, 10). returned that 0 bytes were read.
+closed the fd.
+
+Program №3
+
+my pid = 3441, returned pid = 3442
+my pid = 3442, returned pid = 0
